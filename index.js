623fde700292a4cc74f08c3f9852fe7bdbd72c12
Joao Leitao <jleitao@cloudflare.com>
2022-11-15 14:18:24 +0000

    POST request differentiation

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 export default {
 	fetch(request) {
-		console.log(request.url)
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+		console.log(request.URL)
+		
+		if (request.method == "GET") {
+			return new Response('Hello worker! This is a GET request!', {
+				headers: {'content-type': 'text/plain',},
+
+			});
+		}
+
+		if (request.method == "POST") {
+			return new Response('Hello worker! This is a POST request!', {
+				headers: {'content-type': 'text/plain',},
+			});
+		}
 	},
-};
+}
