4afba4d77d58cfbdd8343d0f4b96e701c39ea229
Joao Leitao <jleitao@cloudflare.com>
2022-11-15 14:27:14 +0000

    Update with logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export default {
 	fetch(request) {
 		console.log(request.URL)
+		console.log("HTTP Method: " + request.method)
 		
 		if (request.method == "GET") {
 			return new Response('Hello worker! This is a GET request!', {
@@ -9,8 +10,14 @@ export default {
 			});
 		}
 
-		if (request.method == "POST") {
+		else if (request.method == "POST") {
 			return new Response('Hello worker! This is a POST request!', {
+				headers: {'content-type': 'text/plain',},
+			});
+		}
+
+		else {
+			return new Response('Hello worker! This is not a GET/POST request!', {
 				headers: {'content-type': 'text/plain',},
 			});
 		}
